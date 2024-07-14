5710ae5fdce567cb89dce53c2d723752ca4ee386
Hyune-c <choi8608@gmail.com>
2024-07-15 01:09:47 +0900

    [pickup 도메인 구현] mysql 모듈 구현: add dependency - p6spy, testcontainer

--- infrastructure/mysql/build.gradle.kts
+++ infrastructure/mysql/build.gradle.kts
@@ -12,6 +12,14 @@ dependencies {
     implementation(project(":common"))
     implementation(project(":domain:pickup"))
 
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    api("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    val testcontainersVersion: String by project
+    implementation("org.testcontainers:testcontainers:${testcontainersVersion}")
+    implementation("org.testcontainers:junit-jupiter:${testcontainersVersion}")
+    implementation("org.testcontainers:mysql:${testcontainersVersion}")
+
+    val p6spyVersion: String by project
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:${p6spyVersion}")
 }
